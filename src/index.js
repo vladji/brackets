1a0a61a63c12a578debd6b1250a7bad463390c01
vladji <vladjislav@gmail.com>
2019-09-24 09:37:25 +0300

    feat: resolve

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if (str[i] === bracketsConfig[j][0] && str[i + 1] === bracketsConfig[j][1]) {
+        str = str.slice(0, i) + str.slice(i + 2, str.length);
+        if (str.length == 1) {
+          return false;
+        } else if (str.length == 0) {
+          return true;
+        }
+        i = -1;
+      }
+    }
+  }
+  return false;
 }
